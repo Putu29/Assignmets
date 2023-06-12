c5300feef6af8f9eba483d7bafdbf6b8970a43d9
Putu29 <130934599+Putu29@users.noreply.github.com>
2023-06-12 21:19:39 +0530

    Add files via upload

--- assignment2.js
+++ assignment2.js
@@ -17,9 +17,40 @@
 //   },
 // ];
 
-const hello = document.getElementById("demo");
-var sum = 0;
+// const hellohtml = "<h2>Hi</h2>";
+// const hellocontainer = document.getElementById("demo");
+// let i = 0;
+// for (i = 0; i < 5; i++) {
+//   const fragment = document.createElement("div"); // <div></div>
+//   fragment.innerHTML = fragment.innerHTML + hellohtml;
+//   hellocontainer.appendChild(fragment);
+// }
+
+// console.log(hellocontainer);
+
+const hellohtml = "<h2>Hi</h2>";
+const hellocontainer = document.getElementById("demo");
 let i = 0;
 for (i = 0; i < 5; i++) {
-  sum = sum + console.log(hello);
+  const fragment = document.createElement("div"); // <div></div>
+  fragment.innerHTML = fragment.innerHTML + hellohtml;
+  hellocontainer.appendChild(fragment);
+  //abc();
 }
+
+function abc() {
+  return hellocontainer + i + hellocontainer;
+}
+// console.log(hellocontainer);
+
+// let i = 0;
+// const meatContainer = document.getElementById("meat-box");
+// btnElement3.addEventListener("click", function () {
+//   // console.log("I was clicked middle!!!");
+//   // middleElement.style.display = isDisplayedStatus3 ? "none" : "block";
+//   // isDisplayedStatus3 = !isDisplayedStatus3;
+//   const fragment = document.createElement("section"); // <div></div>
+//   fragment.innerHTML = meatHtml(i); //<div><div class="middle rectangle-meat"></div></div>
+//   i += 1;
+//   meatContainer.appendChild(fragment);
+// });
